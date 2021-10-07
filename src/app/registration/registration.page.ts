51957c842ef53387ca6569250439a13c944773a3
Cruspoid <80702064+Cruspoid@users.noreply.github.com>
2021-10-07 02:14:47 -0300

    añadido forgf

--- src/app/registration/registration.page.ts
+++ src/app/registration/registration.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.page.html',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationPage implements OnInit {
 
-  constructor() { }
+  constructor(private toastCtrl: ToastController) { 
+
+  }
 
   ngOnInit() {
   }
+  async showToast(){
+    let toast = await this.toastCtrl.create({
+      message: "Cuenta creada con exito",
+      duration: 5000,
+      color: "dark",
+      position: "bottom"
+    });
+    toast.present();
+
+  }
+
 
 }
